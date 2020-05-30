2e7ce5ba5dcb7aade2f9885d94d828cc801f8c91
Alan <alanpjaffe@googlemail.com>
2020-05-30 13:40:13 -0400

    Gunicorn

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,2 @@
-LC_ALL=C.UTF-8 LANG=C.UTF-8 FLASK_APP=server.py python3 -m flask run
+mkdir -p games
+sudo /usr/local/bin/gunicorn --certfile /etc/letsencrypt/live/api.mahj.party/fullchain.pem --keyfile /etc/letsencrypt/live/api.mahj.party/privkey.pem -b 0.0.0.0:443 --workers=1 --timeout 60 -p /tmp/gunicorn.pid --access-logfile logs/gunicorn-access.log --error-logfile logs/gunicorn-error.log server:app
